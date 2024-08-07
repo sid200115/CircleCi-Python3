22ae192c9b0b49ed8ff900ff6c1977b95121b8e5
SiddhantSharma <54277508+sid200115@users.noreply.github.com>
2024-08-07 08:52:33 +0530

    circle Ci practical MSCIT

---
+++ main.py
@@ -0,0 +1,20 @@
+def to_upper(name):
+
+    return name.upper()
+
+
+def say_hello(name):
+
+    print(f'Hello,[name]')
+
+    
+
+if  __name__ =='__main__':
+
+    name='Siddhant'
+
+    say_hello(name)
+
+    up=to_upper(name)
+
+    print(up)
